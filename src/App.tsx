9777166282faf1adc91d2aa735bce48420e2c96a
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2025-01-24 06:16:07 +0100

    auth page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, Login } from "./Routes";
+import { Home, Login, SignUp } from "./Routes";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   );
 };
